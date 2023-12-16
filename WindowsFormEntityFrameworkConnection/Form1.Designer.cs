152ac88084044e8b41be98afbc0dd99790fa7491
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2023-12-17 00:42:28 +0300

    Proje dosyası ekle.

---
+++ WindowsFormEntityFrameworkConnection/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormEntityFrameworkConnection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewPersonel = new System.Windows.Forms.DataGridView();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPersonel
+            // 
+            this.dataGridViewPersonel.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewPersonel.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewPersonel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPersonel.Location = new System.Drawing.Point(12, 64);
+            this.dataGridViewPersonel.Name = "dataGridViewPersonel";
+            this.dataGridViewPersonel.Size = new System.Drawing.Size(776, 343);
+            this.dataGridViewPersonel.TabIndex = 0;
+            this.dataGridViewPersonel.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewPersonel_CellContentClick);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(12, 25);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(257, 20);
+            this.txtAd.TabIndex = 1;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(272, 26);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(257, 20);
+            this.txtSoyad.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Soyad";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 21);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Kayıt Et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 417);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 21);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(180, 417);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 21);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(532, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Telefon";
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(535, 25);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(253, 20);
+            this.txtTelefon.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.dataGridViewPersonel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPersonel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewPersonel;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTelefon;
+    }
+}
+
---
+++ WindowsFormEntityFrameworkConnection/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormEntityFrameworkConnection.Models;
+
+namespace WindowsFormEntityFrameworkConnection
+{
+    public partial class Form1 : Form
+    {
+        WindowsFormEntityFrameworkConnectionDbEntities db = new
+            WindowsFormEntityFrameworkConnectionDbEntities();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridViewPersonel.DataSource = db.Personel.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Kayıt Et
+            Personel p = new Personel();
+            p.Name = txtAd.Text;
+            p.LastName = txtSoyad.Text;
+            p.Telefon = txtTelefon.Text;
+            db.Personel.Add(p);
+            db.SaveChanges();
+            dataGridViewPersonel.DataSource = db.Personel.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Güncelle
+            int id = int.Parse(dataGridViewPersonel.CurrentRow.Cells[0].Value.ToString());
+
+            Personel p = db.Personel.FirstOrDefault(x => x.PersonelId == id);
+            p.Name = txtAd.Text;
+            p.LastName = txtSoyad.Text;
+            p.Telefon = txtTelefon.Text;
+            db.SaveChanges();
+            dataGridViewPersonel.DataSource = db.Personel.ToList();
+        }
+
+        private void dataGridViewPersonel_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtAd.Text = dataGridViewPersonel.CurrentRow.Cells[1].Value.ToString();
+            txtSoyad.Text = dataGridViewPersonel.CurrentRow.Cells[2].Value.ToString();
+            txtTelefon.Text = dataGridViewPersonel.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Sil
+            int id = int.Parse(dataGridViewPersonel.CurrentRow.Cells[0].Value.ToString());
+
+            Personel p = db.Personel.FirstOrDefault(x => x.PersonelId == id);
+            DialogResult sor = new DialogResult();
+            sor = MessageBox.Show($@"{p.Name} {p.LastName} Personelini Kalıcı Olarak Silmek İstediğinize Emin Misiniz?", 
+                "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(sor == DialogResult.Yes)
+            {
+                db.Personel.Remove(p);
+                db.SaveChanges();
+                dataGridViewPersonel.DataSource = db.Personel.ToList();
+                txtAd.Text = txtSoyad.Text = txtTelefon.Text = null;
+            }
+
+            
+        }
+    }
+}
